26c631654c47cb4a894edbc722cd6ac6f57d33c5
warenix <warenix@gmail.com>
2020-06-20 19:13:41 +0800

    Try accept base64 encoded image and return it back to browser

--- api/echo-image.js
+++ api/echo-image.js
@@ -1,12 +1,37 @@
+var fs = require("fs");
+
 exports.handler = async (event) => {
+  let outPath = '/tmp/a.jpg';
   let image = event.body;
+  
+  console.log("event.body", event.body);
+  let j = JSON.parse(event.body);
+  console.log("j", j);
+  console.log("j.image", j.image);
+
+  var buff = new Buffer(j.image
+    .replace(/^data:image\/(png|gif|jpeg);base64,/,''), 'base64');
+  fs.writeFile(outPath, buff, function (err) {
+      console.log('done');
+  });
+
+  // return {
+  //   statusCode: 200,
+  //   headers: {
+  //     "Content-type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     "path": outPath,
+  //   }),
+  // };
 
   return {
     statusCode: 200,
     headers: {
       "Content-type": "image/jpeg",
     },
-    body: image.toString("base64"),
+    body: buff.toString("base64"),
     isBase64Encoded: true,
   };
 };
+
