d3d750200e5162c7f5d8a5d8b9ece54d18f562d6
warenix <warenix@gmail.com>
2020-06-20 19:33:23 +0800

    Add cors header

--- api/echo-image.js
+++ api/echo-image.js
@@ -33,6 +33,7 @@ exports.handler = async (event) => {
       "Content-type": "image/jpeg",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
     },
     body: buff.toString("base64"),
     isBase64Encoded: true,
