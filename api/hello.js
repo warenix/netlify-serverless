ef8fa8d6b5d5ae47cc254c353380d1fd4cfd2751
warenix <warenix@gmail.com>
2020-06-21 07:33:38 +0800

    Test cors header

--- api/hello.js
+++ api/hello.js
@@ -3,6 +3,14 @@ exports.handler = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Content-type": "image/jpeg",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "access-control-allow-origin, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers",
+    },
     body: `Hello ${subject}!`,
   };
 };
