2de1e1d6ea2a7fef8eef6d55e4d56eacacb1b3c8
warenix <warenix@gmail.com>
2020-06-21 07:39:53 +0800

    Test cors header

--- api/echo-image.js
+++ api/echo-image.js
@@ -1,6 +1,26 @@
 var fs = require("fs");
 
 exports.handler = async (event) => {
+  let CORSHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+    "Access-Control-Allow-Headers":
+      "access-control-allow-origin, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers",
+  };
+
+  if (process.env.ENABLE_CORS && event.httpMethod === "OPTIONS") {
+    const response = {
+      statusCode: 200,
+      headers: {
+        CORSHeaders,
+      },
+      body: JSON.stringify({ message: "You can use CORS" }),
+    };
+    callback(null, response);
+    return;
+  }
+
   let outPath = "/tmp/a.jpg";
   let image = event.body;
 
@@ -27,16 +47,12 @@ exports.handler = async (event) => {
   //   }),
   // };
 
+  CORSHeaders[""];
   return {
     statusCode: 200,
-    headers: {
-      "Content-type": "image/jpeg",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      "Access-Control-Allow-Headers":
-        "access-control-allow-origin, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers",
-    },
+    headers: Object.assign({}, CORSHeaders, {
+      "Content-Type": "image/jpeg",
+    }),
     body: buff.toString("base64"),
     isBase64Encoded: true,
   };
