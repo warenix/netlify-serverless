eb31b12a42d47fe4ceb34277326d40da1436f118
warenix <warenix@gmail.com>
2020-06-21 08:32:45 +0800

    Trying to fix timeout error

--- api/echo-image.js
+++ api/echo-image.js
@@ -26,46 +26,63 @@ exports.handler = async (event, context, callback) => {
     return;
   }
 
-  let outPath = "/tmp/a.jpg";
-  let image = event.body;
+  try {
+    let outPath = "/tmp/a.jpg";
+    let image = event.body;
 
-  console.log("event.body", event.body);
-  let j = JSON.parse(event.body);
-  console.log("j", j);
-  console.log("j.image", j.image);
+    console.log("event.body", event.body);
+    let j = JSON.parse(event.body);
+    console.log("j", j);
+    console.log("j.image", j.image);
 
-  var buff = Buffer.from(
-    j.image.replace(/^data:image\/(png|gif|jpeg);base64,/, ""),
-    "base64"
-  );
-  fs.writeFile(outPath, buff, function (err) {
-    console.log("done");
-  });
+    var buff = Buffer.from(
+      j.image.replace(/^data:image\/(png|gif|jpeg);base64,/, ""),
+      "base64"
+    );
+    // fs.writeFile(outPath, buff, function (err) {
+    //   console.log("done");
+    // });
 
-  // return {
-  //   statusCode: 200,
-  //   headers: {
-  //     "Content-type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     "path": outPath,
-  //   }),
-  // };
+    // return {
+    //   statusCode: 200,
+    //   headers: {
+    //     "Content-type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     "path": outPath,
+    //   }),
+    // };
 
-  CORSHeaders[""];
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Content-type": "image/jpeg",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      "Access-Control-Allow-Headers":
-        "access-control-allow-origin, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers",
-    },
-    body: buff.toString("base64"),
-    isBase64Encoded: true,
-  };
-  callback(null, response);
+    CORSHeaders[""];
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Content-type": "image/jpeg",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "access-control-allow-origin, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers",
+      },
+      body: buff.toString("base64"),
+      isBase64Encoded: true,
+    };
+    callback(null, response);
+  } catch (error) {
+    const response = {
+      statusCode: 500,
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "access-control-allow-origin, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers",
+      },
+      body: JSON.stringify({ error: error.message }),
+      isBase64Encoded: false,
+    };
+    callback(null, response);
+  }
   return;
 };
